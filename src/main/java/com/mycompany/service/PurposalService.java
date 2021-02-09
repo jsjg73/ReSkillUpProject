bd3a5d13d083fa1fb681da12a28c72b5f7837249
ckstn123 <ckstn123@cnu.ac.kr>
2021-02-09 14:24:42 +0900

    정해진 경로에 사진을 올려주는 메서드 구현

--- src/main/java/com/mycompany/service/PurposalService.java
+++ src/main/java/com/mycompany/service/PurposalService.java
@@ -1,5 +1,7 @@
 package com.mycompany.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.mycompany.domain.PurposalDTO;
 
 public interface PurposalService {
@@ -7,5 +9,5 @@ public interface PurposalService {
 	int purposalCnt();
 	public void PurposalInsert(PurposalDTO dto);
 	void purposalUpdate(PurposalDTO dto);
-	
+	public String saveFile(MultipartFile file);
 }
--- src/main/java/com/mycompany/service/PurposalServiceImpl.java
+++ src/main/java/com/mycompany/service/PurposalServiceImpl.java
@@ -1,7 +1,11 @@
 package com.mycompany.service;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.mycompany.domain.Criteria;
 import com.mycompany.domain.PurposalDTO;
@@ -26,5 +30,23 @@ public class PurposalServiceImpl implements PurposalService {
 	public void purposalUpdate(PurposalDTO dto) {
 		purDAO.purposalUpdate(dto);
 	}
+	
+	@Override
+	public String saveFile(MultipartFile file){
+	    
+	    String saveName = file.getOriginalFilename();
+
+	    // 저장할 File 객체를 생성(껍데기 파일)ㄴ
+	    File saveFile = new File("C:\\upload\\tmp",saveName); // 저장할 폴더 이름, 저장할 파일 이름
+
+	    try {
+	        file.transferTo(saveFile); // 업로드 파일에 saveFile이라는 껍데기 입힘
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        return null;
+	    }
+
+	    return saveName;
+	} 
 
 }
