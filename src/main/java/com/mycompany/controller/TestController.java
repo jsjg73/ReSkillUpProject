6d7e6f28b4c4ac4f0e09e182822bc8d7fa532eaf
jae-sung kim <kjs930703@gmail.com>
2021-02-09 21:06:02 +0900

    map 객체 반환

--- src/main/java/com/mycompany/controller/TestController.java
+++ src/main/java/com/mycompany/controller/TestController.java
@@ -1,6 +1,14 @@
 package com.mycompany.controller;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.MediaType;
@@ -24,9 +32,26 @@ public class TestController {
 		return "안녕하세요";
 	}
 	
-	@RequestMapping(value = "/getSample", produces = "application/json")
+	@RequestMapping(value = "/getSample")
 	@ResponseBody
 	public SampleVO getSample() {
 		return new SampleVO(112,"스타","로드");
+	}
+	
+	@RequestMapping(value = "/getList")
+	@ResponseBody
+	public List<SampleVO> getList() {
+		List<SampleVO> list = new ArrayList<SampleVO>();
+		for (int i = 0; i < 10; i++) {
+			list.add(new SampleVO(i, i+"First", i+" Last"));
+		}
+		return list;
+	}
+	@RequestMapping(value = "/getMap")
+	@ResponseBody
+	public Map<String, SampleVO> getMap() {
+		Map<String, SampleVO> map = new HashMap();
+		map.put("First", new SampleVO(111, "그루트", "주니어"));
+		return map;
 	}
 }
