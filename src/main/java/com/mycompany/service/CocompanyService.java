4c4bf0cd02d7e3bf87131ca43ef2d7bc5d6674a7
ckstn123 <ckstn123@cnu.ac.kr>
2021-02-09 17:22:40 +0900

    제조사 service interface 구현

---
+++ src/main/java/com/mycompany/service/CocompanyService.java
@@ -0,0 +1,22 @@
+package com.mycompany.service;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.mycompany.domain.Criteria;
+import com.mycompany.domain.CocompanyDTO;
+
+public interface CocompanyService {
+	int cocompanyCnt();
+	public void cocompanyInsert(CocompanyDTO dto);
+	void cocompanyUpdate(CocompanyDTO dto);
+
+
+	List<CocompanyDTO> cocompanyList();
+	List<CocompanyDTO> cocompanyListPaging(Criteria cri);
+
+	CocompanyDTO cocompanyRead(CocompanyDTO dto);
+
+	public String saveFile(MultipartFile file);
+}
