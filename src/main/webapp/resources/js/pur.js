f5f4e712f35f234dbc0502670a3204c497646cd3
ckstn123 <ckstn123@cnu.ac.kr>
2021-02-10 23:22:48 +0900

    여러 예외 조건 추가

--- src/main/webapp/resources/js/pur.js
+++ src/main/webapp/resources/js/pur.js
@@ -1,5 +1,6 @@
 $(function(){
 	$("#pur_writeform").submit(function(){
+		var flag = false;
 		if( $("#pdt_name").val() == "" ) {
 			alert("제품명를 꼭 입력해주세요!");
 			$("#pdt_name").focus();
@@ -10,33 +11,32 @@ $(function(){
 			$("#reg_date").focus();
 			return false;
 		}
-		if( pwf.target[0].checked == false &&
-		pwf.target[1].checked == false &&
-		pwf.target[2].checked == false &&
-		pwf.target[3].checked == false 	) {
-			alert("주 고객층을 꼭 선택해주세요!");
+		if($("[name='pdt_type'] :selected").val()== "none"){
+			alert("유형을 꼭 선택해주세요!");
 			return false;
 		}
-		if( $("#pdt_type").val() =="") {
-			alert("유형을 꼭 선택해주세요!");
+		if($("input:checkbox[name='target']").is(":checked") == true){
+				flag = true;
+		}
+		if( !flag ) {
+			alert("고객층을 꼭 선택해주세요!");
 			return false;
 		}
 		
 	});
 	
-	$("#pur_updateform").submit(function(){
-	   
+	$("#pur_updateform").submit(function(){	
+		var flag = false;
 		if( $("#reg_date").val() == "" ) {
 			alert("기획날짜를 꼭 선택해주세요!");
 			$("#reg_date").focus();
 			return false;
 		}
-		if( puf.target[0].checked == false &&
-				puf.target[1].checked == false &&
-				puf.target[2].checked == false &&
-				puf.target[3].checked == false &&
-				puf.target[4].checked == false 	) {
-			alert("주 고객층을 꼭 선택해주세요!");
+		if($("input:checkbox[name='target']").is(":checked") == true){
+				flag = true;
+		}
+		if( !flag ) {
+			alert("고객층을 꼭 선택해주세요!");
 			return false;
 		}
 	});
