4baae62d1db9f90c597e223060378eebe8dbe9cc
ckstn123 <ckstn123@cnu.ac.kr>
2021-02-10 15:59:01 +0900

    uploadService에 추가 기능 구현

--- src/main/java/com/mycompany/service/UploadService.java
+++ src/main/java/com/mycompany/service/UploadService.java
@@ -2,6 +2,8 @@ package com.mycompany.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -9,21 +11,32 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class UploadService {
-	public String saveFile(MultipartFile file) {
-
+	public String saveFiles(MultipartFile file, Date date) {
+		File uploadPath = new File("C:\\upload\\tmp",getFolder(date));
+		
+		if(uploadPath.exists()==false)
+			uploadPath.mkdirs();
+		//make yyyy/MM/dd folder
 		UUID uuid = UUID.randomUUID();
-		String saveName = uuid + "_" + file.getOriginalFilename();
+		
+		String saveName = uuid.toString()+"_"+ file.getOriginalFilename();
+	    // 저장할 File 객체를 생성(껍데기 파일)
+	    File saveFile = new File(uploadPath,saveName); // 저장할 폴더 이름, 저장할 파일 이름
 
-		// 저장할 File 객체를 생성(껍데기 파일)
-		File saveFile = new File("C:\\upload\\tmp", saveName); // 저장할 폴더 이름, 저장할 파일 이름
-
-		try {
-			file.transferTo(saveFile); // 업로드 파일에 saveFile이라는 껍데기 입힘
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
+	    try {
+	        file.transferTo(saveFile); // 업로드 파일에 saveFile이라는 껍데기 입힘
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        return null;
+	    }
 
 		return saveName;
+	}
+	
+	
+	private String getFolder(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String str = sdf.format(date);
+		return str.replace("-", File.separator);
 	}
 }
