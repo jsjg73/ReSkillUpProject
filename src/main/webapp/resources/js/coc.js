634a68bfc8b246602e7c75b33fe065a4b010074f
ckstn123 <ckstn123@cnu.ac.kr>
2021-02-11 23:09:06 +0900

    샘플ID 중복체크하는 function 추가

--- src/main/webapp/resources/js/coc.js
+++ src/main/webapp/resources/js/coc.js
@@ -46,5 +46,46 @@ $(function(){
 		}
 	});
 	
+	//제조사명 중복검사
+	var coc_name ="";
+	$("#coc_duplicate").on("click",function(){
 	
+		//중복검사한 제조사명 저장
+		coc_name = $("#coc_name").val().trim();
+		
+		var pattern2 = /[`~!@#$%^&*|\\\'\";:\/?]/;
+		
+		if(coc_name=="" || pattern2.test(coc_name)){
+			alert("사용 불가능한 제조사명임(특수문자 확인바람.)");
+			$("#coc_nametd").text("올바른 제조사명 입력바람");
+			$("#coc_name").focus();
+			return false;
+		}
+		
+		$.ajax({
+		 	url: '/coc_dupli',
+		    type: 'GET',
+		    dataType: 'json', //서버로부터 내가 받는 데이터의 타입
+		    contentType : 'application/json',//내가 서버로 보내는 데이터의 타입
+		    data: {coc_name : coc_name},
+
+		    success: function(data){
+		         if(data == -1){
+		         	alert("사용가능한 제조사명입니다.");
+		         	//$("#coc_nametd").text(coc_name + " 은(는) 사용가능한 제조사명 ");
+		         }else{
+		         	alert("제조사명 중복");
+		         	$("#coc_name").val("").focus();
+		         	$("#coc_nametd").text("");
+		         	pdt_name="";
+		         	return false;
+		         }
+		    },
+		    error: function (){        
+		        alert("ajax 에러")
+		    }
+		});//ajax end
+		
+		return false;//submit 방지
+	});
 });
