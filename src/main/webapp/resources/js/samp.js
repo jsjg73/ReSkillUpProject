87e639dffff81276748bd289fb71af0fbe3a275b
ckstn123 <ckstn123@cnu.ac.kr>
2021-02-11 22:58:43 +0900

    samp에서 중복체크하는 function 추가

--- src/main/webapp/resources/js/samp.js
+++ src/main/webapp/resources/js/samp.js
@@ -38,4 +38,47 @@ $(function(){
 			$("#price").focus();
 		}
 	});
+	
+	//제품명 중복검사
+	var samp_id ="";
+	$("#samp_duplicate").on("click",function(){
+	
+		//중복검사한 제품명 저장
+		samp_id = $("#samp_id").val().trim();
+		
+		var pattern2 = /[`~!@#$%^&*|\\\'\";:\/?]/;
+		
+		if(samp_id=="" || pattern2.test(samp_id)){
+			alert("사용 불가능한 샘플ID임(특수문자 확인바람.)");
+			$("#samp_idtd").text("올바른 샘플ID 입력바람");
+			$("#samp_id").focus();
+			return false;
+		}
+		
+		$.ajax({
+		 	url: '/samp_dupli',
+		    type: 'GET',
+		    dataType: 'json', //서버로부터 내가 받는 데이터의 타입
+		    contentType : 'application/json',//내가 서버로 보내는 데이터의 타입
+		    data: {samp_id : samp_id},
+
+		    success: function(data){
+		         if(data == -1){
+		         	alert("사용가능한 샘플ID입니다.");
+		         	//$("#samp_idtd").text(samp_id + " 은(는) 사용가능한 제품명 ");
+		         }else{
+		         	alert("샘플ID 중복");
+		         	$("#samp_id").val("").focus();
+		         	$("#samp_idtd").text("");
+		         	samp_id="";
+		         	return false;
+		         }
+		    },
+		    error: function (){        
+		        alert("ajax 에러")
+		    }
+		});//ajax end
+		
+		return false;//submit 방지
+	});
 });
