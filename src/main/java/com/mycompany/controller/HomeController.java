d97c803ddf8aa3ff1fc90ff0eaa62d19e78ea1a6
ckstn123 <ckstn123@cnu.ac.kr>
2021-02-15 13:32:25 +0900

    로그인 메서드 추가

--- src/main/java/com/mycompany/controller/HomeController.java
+++ src/main/java/com/mycompany/controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.mycompany.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mycompany.domain.CocompanyDTO;
+import com.mycompany.service.CocompanyService;
 
 /**
  * Handles requests for the application home page.
@@ -14,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 
 public class HomeController {
+	@Autowired
+	CocompanyService coc_service;
 	
 	@RequestMapping("/")
 	public String index() {
@@ -23,7 +31,27 @@ public class HomeController {
 	public String sessionnull() {
 		return "sessionerror";
 	}
-	
+	@RequestMapping(value = "home/coc_login", method = RequestMethod.GET)
+	public String coc_loginform(HttpSession sess,Model model) {
+		
+		return "coc_loginform";
+	}
+	@RequestMapping(value = "home/coc", method = RequestMethod.POST)
+	public String coc_login(HttpSession sess, String coc_name, String pwd) {
+		List<CocompanyDTO> list = coc_service.cocompanyRead(coc_name);
+		if(list.isEmpty()) {
+			return "login_fail";
+		}
+		if(list.get(0).getPwd().equals(pwd)) {
+			sess.setAttribute("login", "coc");
+			sess.setAttribute("coc_name", coc_name);
+			return "home";
+		}
+		else {
+			return "login_fail";
+		}
+		
+	}
 	@RequestMapping(value = "home/{login}", method = RequestMethod.GET)
 	public String homeAdmin(HttpSession sess,Model model, @PathVariable("login") String login) {
 		
@@ -35,5 +63,7 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	
 	
 }
